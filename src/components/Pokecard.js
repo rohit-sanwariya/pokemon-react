abe0205130b6b6cf767333e74cfee4f805554ca7
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-01-25 13:21:01 +0530

    Pokecard comp created

---
+++ src/components/Pokecard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class Pokecard extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Pokecard</h1>
+            </div>
+        )
+    }
+}
+export default Pokecard;
