a4573e3e169d358db2638a5e89021923d572762c
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-01-25 14:09:28 +0530

    Pokedex added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Pokecard from './components/Pokecard'
-
+import './App.css'
+import Pokedex from './components/Pokedex'
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Pokecard/>
+      <div className="App">
+        <Pokedex/>
       </div>
     )
   }
--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-
+import '../styles/Pokecard.css'
+const POKE_API ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+const REMAINING = "${id}.png."
  class Pokecard extends Component {
     render() {
+        const imgSrc = `${POKE_API}${this.props.id}.png`
         return (
-            <div>
-                <h1>Pokecard</h1>
+            <div className={"Pokecard"}>
+                <h1>{this.props.name}</h1>
+                <img src={imgSrc} alt="bulbasuar"/>
+                <div>Type: {this.props.type}</div>
+                <div>Exp: {this.props.exp}</div>
             </div>
         )
     }
