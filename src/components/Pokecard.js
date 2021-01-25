f9d1b83f1c131e02d1ae9b2be5e46e456a880611
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-01-25 15:51:22 +0530

    fancier images added and pad function

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import '../styles/Pokecard.css'
-const POKE_API ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
- 
+// const POKE_API ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+const POKE_API ="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
+let padToThree = (number) =>{return (number<999)?`00${number}`.slice(-3):number}
  class Pokecard extends Component {
+     
     render() {
-        const imgSrc = `${POKE_API}${this.props.id}.png`
+        const imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`
         return (
             <div className={"Pokecard"}>
-                <h1>{this.props.name}</h1>
+                <h1 className="Pokecard-title">{this.props.name}</h1>
                 <img src={imgSrc} alt="bulbasuar"/>
-                <div>Type: {this.props.type}</div>
-                <div>Exp: {this.props.exp}</div>
+                <div className="Pokecard-data" >Type: {this.props.type}</div>
+                <div className="Pokecard-data" >Exp: {this.props.exp}</div>
             </div>
         )
     }
